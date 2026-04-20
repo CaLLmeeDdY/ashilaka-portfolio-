088e8c81a9bb37ddad09f976f6c9fe2b12fb3a0d
CaLLmeeDdY <ashilakaedwin@gmail.com>
2026-04-20 03:19:33 +0300

    Deploy Netash_OS

---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+<a 
+  href="/Netash_OS_v3.8.0.apk" 
+  download="Netash_OS_v3.8.0.apk" 
+  className="neon-btn"
+>
+  {/* Terminal / Download Icon */}
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/>
+  </svg>
+  DOWNLOAD NETASH_OS [APK]
+</a>
